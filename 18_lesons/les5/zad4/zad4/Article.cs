132f3dcab6144df2636f4e43a7c9f9f6e809f017
VladNavo <133175669+VladNavo@users.noreply.github.com>
2024-05-01 23:21:09 +0300

    .

---
+++ 18_lesons/les5/zad4/zad4/Article.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad4
+{
+    internal class Article
+    {
+        private string name;
+        private string shopname;
+        private double price;
+
+       
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Shopname
+        {
+            get { return shopname; }
+            set { shopname = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public Article(string name, string shopname, double price)
+        {
+            this.name = name;
+            this.shopname = shopname;
+            this.price = price;
+        }
+
+    }
+}
