d7b5178964b4c51951534656a76715a30dbb4746
VladNavo <133175669+VladNavo@users.noreply.github.com>
2024-05-03 12:12:03 +0300

    .

---
+++ 18_lesons/les9/zad2/zad2/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class Calculator
+    {
+        public  delegate void Operation(int x, int y);
+        public Operation sum = (x, y) => Console.WriteLine($"x:{x}+y:{y}={ x + y}");
+        public Operation substraction = (x, y) => Console.WriteLine($"{x}-{y}= {x - y}");
+        public Operation multiplication = (x, y) => Console.WriteLine($"{x}*{y}= {x * y}");
+        public delegate void OperationDivision(double x, double y);
+        public OperationDivision division = (x, y) =>
+        {
+            if (y != 0)
+            {
+                Console.WriteLine($"{x}/{y}={x / y}");
+            }
+            else
+            {
+                Console.WriteLine("на 0 нельзя");
+            }
+        };
+    }
+}
---
+++ 18_lesons/les9/zad2/zad2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("введите 1 число:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("введите 2 число:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            calculator.sum(x, y);
+            calculator.substraction(x, y);
+            calculator.multiplication(x, y);
+            calculator.division(x, y);
+
+        }
+    }
+}
