1cb40bc314dc03c8f532c4959a9745dc81d33f6b
VladNavo <133175669+VladNavo@users.noreply.github.com>
2024-05-03 11:45:26 +0300

    .

---
+++ 18_lesons/les8/zad2/zad2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace zad2
+{
+    internal class Program
+    {
+        enum Colors
+        {
+            White,
+            Red,
+            Green,
+            Yellow,
+            Blue,
+            Magenta,
+            Cyan
+        }
+
+        static class Printer
+        {
+            public static void Print(string stroka, Colors color)
+            {
+                switch (color)
+                {
+                    case Colors.White:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case Colors.Red:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case Colors.Green:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case Colors.Yellow:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case Colors.Blue:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    case Colors.Magenta:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    case Colors.Cyan:
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                }
+                Console.WriteLine(stroka);
+                Console.ResetColor();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выбор цвета (White, Red, Green, Yellow, Blue, Magenta, Cyan): (0, 1, 2, 3, 4, 5, 6)");
+            int indexOfColor = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите само сообщение:");
+            string textMessage = Console.ReadLine();
+
+            if (Enum.IsDefined(typeof(Colors), indexOfColor))
+            {
+                Printer.Print(textMessage, (Colors)indexOfColor);
+            }
+            else
+            {
+                Console.WriteLine("Неверный выбор цвета. Используйте числа от 0 до 6.");
+            }
+        }
+    }
+}
