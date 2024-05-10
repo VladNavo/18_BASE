e3702c3955526c9b61b451f567c16c36b5da0540
VladNavo <133175669+VladNavo@users.noreply.github.com>
2024-05-11 01:51:06 +0300

    11.05

---
+++ 18_lesons/les18/zad2/zad2.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Experement exp= new Experement();
+            exp.nummber = 1;
+            Console.WriteLine("exp.number"+exp.nummber);
+        }
+    }
+}
