a12e490db5aa41396374834c4b4119bdd57e4950
VladNavo <133175669+VladNavo@users.noreply.github.com>
2024-05-11 01:17:05 +0300

    11.05

---
+++ 18_lesons/les11/zad3/zad3/Dictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad3
+{
+    internal class Dictionary<TKey,TValue>
+    {
+        private List<KeyValuePair<TKey, TValue>> items;
+
+        public int Count
+        {
+            get { return items.Count; }
+        }
+
+        public Dictionary()
+        {
+            items = new List<KeyValuePair<TKey, TValue>>();
+        }
+
+        public void Add(TKey key, TValue value) 
+        {
+            items.Add(new KeyValuePair<TKey, TValue>(key, value));
+        }
+        public TValue this[TKey key]
+        {
+            get
+            {
+                foreach (var item in items)
+                {
+                    if(EqualityComparer<TKey>.Default.Equals(item.Key, key))
+                    {
+                        return item.Value;
+                    }
+                    
+                }
+                throw new KeyNotFoundException($"Key '{key}' not found in dictionary.");
+            }
+        }
+    }
+}
---
+++ 18_lesons/les11/zad3/zad3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+            myDictionary.Add("one", 1);
+            myDictionary.Add("two", 2);
+            myDictionary.Add("three", 3);
+
+            Console.WriteLine($"Value for key 'one': {myDictionary["one"]}");
+            Console.WriteLine($"Value for key 'two': {myDictionary["two"]}");
+            Console.WriteLine($"Value for key 'three': {myDictionary["three"]}");
+
+            Console.WriteLine($"Total number of elements: {myDictionary.Count}");
+        }
+    }
+}
