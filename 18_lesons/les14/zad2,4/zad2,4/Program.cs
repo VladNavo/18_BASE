bca58c91d7b20b1d204f1a964f9f14b4018d1bcd
VladNavo <133175669+VladNavo@users.noreply.github.com>
2024-05-11 00:56:10 +0300

    11.05

---
+++ 18_lesons/les14/zad2,4/zad2,4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+
+            foreach (int item in myList)
+            {
+                Console.WriteLine(item);
+            }
+            
+            var array = MyListExtension.GetArray(myList);
+            foreach (var item in array)
+            {
+                Console.WriteLine($"array item:"+item);
+            }
+
+
+
+        }
+    }
+}
